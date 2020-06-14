9acea83b77ff2123405cf226624a6363c92e0035
Dmytro Cisar <dmytro.cisar@gmail.com>
2020-06-14 13:38:22 +0300

    fix codestyle

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const postcss = require('postcss');
 
 const propDict = {
-    'border-top-right-radius': 'border-radius',
-    'border-top-left-radius': 'border-radius',
-    'border-bottom-right-radius': 'border-radius',
-    'border-bottom-left-radius': 'border-radius',
+  'border-top-right-radius': 'border-radius',
+  'border-top-left-radius': 'border-radius',
+  'border-bottom-right-radius': 'border-radius',
+  'border-bottom-left-radius': 'border-radius',
 
-    'border-left-width': 'border-width',
-    'border-right-width': 'border-width',
-    'border-top-width': 'border-width',
-    'border-bottom-width': 'border-width',
+  'border-left-width': 'border-width',
+  'border-right-width': 'border-width',
+  'border-top-width': 'border-width',
+  'border-bottom-width': 'border-width',
 
-    'margin-left': 'margin',
-    'margin-right': 'margin',
-    'margin-top': 'margin',
-    'margin-bottom': 'margin',
+  'margin-left': 'margin',
+  'margin-right': 'margin',
+  'margin-top': 'margin',
+  'margin-bottom': 'margin',
 
-    'padding-left': 'padding',
-    'padding-right': 'padding',
-    'padding-top': 'padding',
-    'padding-bottom': 'padding',
+  'padding-left': 'padding',
+  'padding-right': 'padding',
+  'padding-top': 'padding',
+  'padding-bottom': 'padding',
 
-    'overflow-x': 'overflow',
-    'overflow-y': 'overflow',
-}
+  'overflow-x': 'overflow',
+  'overflow-y': 'overflow',
+};
 
 function getPropKey (prop) {
-    if (!prop) return;
-    return propDict[prop] || prop;
+  if (!prop) return;
+  return propDict[prop] || prop;
 }
 
 module.exports = postcss.plugin('postcss-tailwind-comments', function (opts) {
@@ -41,9 +41,9 @@ module.exports = postcss.plugin('postcss-tailwind-comments', function (opts) {
       const prevKey = getPropKey(prevProp);
 
       if (!prevKey) {
-          rule.raws.before = `/* ${key} */\n`
+        rule.raws.before = `/* ${key} */\n`;
       } else if (prevKey !== key) {
-          rule.raws.before = `\n/* ${key} */\n`
+        rule.raws.before = `\n/* ${key} */\n`;
       }
     });
   };
